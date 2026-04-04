67173e06a202b8b25545a87e1d83870b74066833
saivara1341 <144797272+saivara1341@users.noreply.github.com>
2026-04-04 15:31:10 +0000

    Deploying to gh-pages from @ saivara1341/inkfinity@f78bc1bf771d8c5ec366389c97148b4a7222d446 🚀

---
+++ assets/camera-DlwGBmZT.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-DfOkKtZr.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("Briefcase",[["path",{d:"M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"jecpp"}],["rect",{width:"20",height:"14",x:"2",y:"6",rx:"2",key:"i6l2r4"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("Camera",[["path",{d:"M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z",key:"1tc9qg"}],["circle",{cx:"12",cy:"13",r:"3",key:"1vg3eu"}]]);export{c as B,r as C};
